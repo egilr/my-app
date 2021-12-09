c0efd6bcabe16078e469cff3a071d4f775ae0c66
Egil Rød Larsen <egil@erl.dk>
2021-12-09 22:16:52 +0100

    Updates

---
+++ precache-manifest.1279166ae970fc55f77de995db137a8a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56c1942bdfe5df18e118397881f76322",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "d371bd24568916e1c0d4",
+    "url": "/my-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7a9b35a47992e7388bfb",
+    "url": "/my-app/static/js/2.e8286c5e.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/my-app/static/js/2.e8286c5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f17882e3e9299f21a016",
+    "url": "/my-app/static/js/3.7c72e190.chunk.js"
+  },
+  {
+    "revision": "d371bd24568916e1c0d4",
+    "url": "/my-app/static/js/main.ec6a584a.chunk.js"
+  },
+  {
+    "revision": "dd1e7e7fa8296d657b7b",
+    "url": "/my-app/static/js/runtime-main.67e01d68.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/my-app/static/media/logo.06e73328.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/egilr/my-app.git/precache-manifest.65e045edb391a0a529549f6f03ffaa2e.js"
+  "/my-app/precache-manifest.1279166ae970fc55f77de995db137a8a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/egilr/my-app.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
